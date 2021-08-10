19c91c19e99eaebc5dec0f13d40e9153fb2ef8ac
Sam :D <SamTF@users.noreply.github.com>
2021-08-10 17:29:09 +0100

    added bar charts

--- graph_dealer.py
+++ graph_dealer.py
@@ -95,3 +95,49 @@ def plot_line_chart(data: pd.DataFrame, file_name: str, title: str, xlabel: str 
     plt.close('all')
 
     return file_name
+
+
+def plot_bar_chart(data: pd.DataFrame, file_name: str, title: str, xlabel: str = '# Of Trades', ylabel: str = '₡urrency Things', date_index:bool = False) -> str:
+    '''
+    Generic plotting function for a BAR chart. Plots a DataFrame with the index as the X values and the SIZE column as the Y values.
+    Save the graph as an SVG and returns the its file name.
+
+    data: Pandas DataFrame with the values to plot. Must contain a SIZE column.
+    file_name: Name to save the SVG as.
+    title: The title of the graph.
+    xlabel: Label for the X-axis.
+    ylabel: Label for the Y-axis.
+    date_index: Whether the x-axis values are dates. 
+    '''
+    # setting the graph size
+    plt.figure(figsize=(8,6), dpi=150)
+
+    # formats dates nicely if the axis values are indeed dates - CONVERTING IT TO STRING REMOVES ALL THE EMPTY SPACE BETWEEN VALUES
+    xaxis = data.index.astype(str) if not date_index else format_dates(data.index)
+
+    # plotting the values 
+    plt.bar(xaxis, data['SIZE'], color = 'xkcd:hot pink')
+
+    # Titles
+    plt.title(title,    fontdict={'fontname': 'Comic Sans MS', 'fontsize': 20, 'color': 'white'})
+    plt.xlabel(xlabel,  fontdict={'fontname': 'Comic Sans MS', 'fontsize': 12, 'color': 'white'})
+    plt.ylabel(ylabel,  fontdict={'fontsize': 12, 'color': 'white'})
+
+    # Customising Colours                        - background, borders, tick markers
+    ax = plt.gca()                               # gets the axes somehow, i dunno what this does
+    ax.set_facecolor('black')                    # background colour
+    ax.spines['bottom'].set_color('white')       # x-axis border colour
+    ax.spines['left'].set_color('white')         # y-axis border colour
+    ax.tick_params(colors="white", labelsize=10) # colour and size of the tick markers
+
+    # Amount of ticks
+    if date_index:
+        ax.xaxis.set_major_locator(plt.MaxNLocator(6))  # sets a maximum amount of 6 ticks on the X Axis
+
+    # Saving the graph as an SVG
+    plt.savefig(FILE_PATH.format(file_name), transparent=True)
+
+    # Closing any open figures from memory
+    plt.close('all')
+
+    return file_name
