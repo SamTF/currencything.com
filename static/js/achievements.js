a9eb2c22b223d5eff841f7a8aa70ab0c130ebc0e
Sam :D <SamTF@users.noreply.github.com>
2021-08-11 00:58:17 +0100

    achievements.js now highlights milestones on user's profile

--- static/js/achievements.js
+++ static/js/achievements.js
@@ -1,17 +1,18 @@
-//// FLAWLESS Method to highlight achievement milestones!
+//// Highlight achievement milestones!
+// Uses two methods: a FLAWLESS one with 100% accuracy, and a HACKY one as backup that just does a ctrl+F if the other method is not available
 // Using:   Python to get the achievement data
 //          JQuery to read the python data, and add the Achievement class to the matching table rows
 
+var method = document.currentScript.getAttribute("data-method")             // Gets the method specified in the HTML file's script tag
 
-var table = $(".table-container");                      // getting the table
-var row_count = $(".table-container tr").length -1;     // getting amount of rows in table (minus 1 because it starts at 0)
+var table = $(".table-container");                                          // getting the table
+var row_count = $(".table-container tr").length -1;                         // getting amount of rows in table (minus 1 because it starts at 0)
 
 // Getting the Achievement Milestone & Trade ID values from Python as an array : [ACHIEVEMENT, TRADE ID]
 $.getJSON({
-        url: "/get_achievements",                       // the app.route from Flask that routes to the desired Python function
+        url: "/get_achievements",                                           // the app.route from Flask that routes to the desired Python function
         success: function(data){
-            // Highlighting each achievement in the HTML data using this data
-            data.forEach(item => highlight_achievement(item));
+            data.forEach(item => highlight_achievement(item));              // Highlighting each achievement in the HTML data using this data
         }
     });
 
@@ -20,16 +21,29 @@ function highlight_achievement(item) {
     achievement = item[0];
     trade_id    = item[1];
 
-    index = row_count - trade_id;                       // since the table is in reverse, we must reverse the index too
-    row = table.find('tr').eq(index);                   // finds the row with matching corrected index
-    row.addClass("achievement");                        // adds the achievement css class
+    row = find_row(trade_id, method)
+
+    row.addClass('achievement')
     
-    desc = `🏆 Achievement! 🎇 The ${achievement}th Currency Thing! 🥳🎉`   // the description displayed on the achievement tooltip
-    row.attr('data-achievement', desc)                  // adds the description as an attribute to be read in the CSS content property
+    desc = `🏆 Achievement! 🎇 The ${achievement}th Currency Thing! 🥳🎉`      // the description displayed on the achievement tooltip
+    row.attr('data-achievement', desc)                                          // adds the description as an attribute to be read in the CSS content property
+
+    console.log(`Search type: ${method}`)
+    console.log(row)
 }
 
+// Finding the wanted row
+function find_row(trade_id, search_type) {
+    // FLAWLESS method - finds table row by table ID
+    if (search_type == "flawless") {
+        index = row_count - trade_id;                                           // since the table is in reverse, we must reverse the index too
+        row = table.find('tr').eq(index);                                       // finds the row with matching corrected index
+    }
 
-
-// Old method here in case it becomes useless for something else
-// kinda hacked method but simple and effective -> https://stackoverflow.com/questions/6135665/jquery-find-table-row-containing-table-cell-containing-specific-text
-// $("tr:contains('143')").addClass('gold');
+    // HACKED method - just does a ctrl+F and returns the first match
+    else {
+        row = $(`tr:contains('${trade_id}')`)
+    }
+    
+    return row
+}
